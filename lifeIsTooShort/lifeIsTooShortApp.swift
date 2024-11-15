b98757cabd7fe506dd8c55f8746af7342a00d47f
carrie <771685809@qq.com>
2024-11-15 15:15:19 +0800

    Initial Commit

---
+++ lifeIsTooShort/lifeIsTooShortApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifeIsTooShortApp.swift
+//  lifeIsTooShort
+//
+//  Created by  玉城 on 2024/11/15.
+//
+
+import SwiftUI
+
+@main
+struct lifeIsTooShortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
